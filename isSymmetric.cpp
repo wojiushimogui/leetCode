ce826b1777f86ef53c5e75f9ff41f4ba4c6f44ef
wuranghao <154943046@qq.com>
2016-01-16 21:36:48 +0800

    is symmetric

---
+++ isSymmetric.cpp
@@ -0,0 +1,19 @@
+
+ struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+  };
+ 
+/*
+思路：将左子树的左节点的值与右子树的右节点的值进行比较，而将左子树的右节点的值与右子树的左节点的值进行比较
+*/
+bool isSym(struct TreeNode* leftroot,struct TreeNode* rightroot){
+	if(leftroot==NULL&&rightroot==NULL){
+        return true;
+    }
+    return (leftroot->val==rightroot->val)&&isSym(leftroot->left,rightroot->right)&&isSym(leftroot->right,rightroot->left);
+}
+bool isSymmetric(struct TreeNode* root) {
+    return isSym(root,root) ; 
+}
