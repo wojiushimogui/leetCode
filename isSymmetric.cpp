9fbe132f2e98dfb36b74e7f749e6eea0683ffdc7
wuranghao <154943046@qq.com>
2016-01-16 21:37:40 +0800

    is symmetric

--- isSymmetric.cpp
+++ isSymmetric.cpp
@@ -12,6 +12,10 @@ bool isSym(struct TreeNode* leftroot,struct TreeNode* rightroot){
 	if(leftroot==NULL&&rightroot==NULL){
         return true;
     }
+    else if(leftroot==NULL&&rightroot!=NULL||(leftroot!=NULL&&rightroot==NULL)) {
+    	return false;
+	}
+    
     return (leftroot->val==rightroot->val)&&isSym(leftroot->left,rightroot->right)&&isSym(leftroot->right,rightroot->left);
 }
 bool isSymmetric(struct TreeNode* root) {
