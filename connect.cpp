59feae26ddafc46212e9ff7438daa9be1ede1552
wuranghao <154943046@qq.com>
2016-02-27 21:31:26 +0800

    pascal triangle

---
+++ connect.cpp
@@ -0,0 +1,15 @@
+
+
+/**
+ * Definition for binary tree with next pointer.*/
+struct TreeLinkNode {
+   int val;
+   struct TreeLinkNode *left, *right, *next;
+};
+ 
+/*
+思路：借助于两个队列来完成 
+*/
+void connect(struct TreeLinkNode *root) {
+    
+}
